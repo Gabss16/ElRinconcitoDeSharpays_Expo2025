cffaef9942afd768997335c3846b67a1b23a6934
imanollramirez <aleimanolramirez@gmail.com>
2025-06-13 09:53:09 -0600

    Register started

--- frontend/sharpays/src/App.jsx
+++ frontend/sharpays/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import "./App.css"
 import Sidebar from './components/Sidebar';
 import Login from "./pages/Login.jsx";
+import Register from "./pages/Register.jsx";
 import Uploadimage from "./pages/Addusers.jsx"
 
 
@@ -15,6 +16,7 @@ return (
 
 <Routes>
     <Route path="/Login" element={<Login/>} />
+    <Route path="/Register" element={<Register/>} />
      <Route path="/Users" element={<Uploadimage/>} />
 </Routes>
 </Router>
---
+++ frontend/sharpays/src/pages/Register.jsx
@@ -0,0 +1,9 @@
+const Register = () => {
+    return(
+        <>
+        <h1 className="text-center">Register</h1>
+        </>
+    );
+};
+
+export default Register;
