cd4a9b2276089c24b8b2a67918c35505e5785d34
imanollramirez <aleimanolramirez@gmail.com>
2025-07-08 21:00:15 -0600

    Profile in progress

---
+++ frontend/private/src/pages/Profile.jsx
@@ -0,0 +1,26 @@
+const Profile = () => {
+    return (
+        <>
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-2"></div>
+                    <div className="col-10">
+
+                        <div
+                            className="position-absolute top-50 start-50"
+                            style={{
+                                borderRadius: 25,
+                                padding: '20px',
+                                boxShadow: '0px 6px 0px 2px rgba(0, 0, 0, 0.14)'
+                            }}>
+                            <b>profile in progress</b>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Profile;
