0cc5b0b8f1c141fccc5ce1e7c3f035757d30204e
imanollramirez <aleimanolramirez@gmail.com>
2025-07-09 20:44:11 -0600

    Cats loaded from the DB in Dashboard

--- frontend/private/src/pages/Dashboard.jsx
+++ frontend/private/src/pages/Dashboard.jsx
@@ -13,16 +13,19 @@ import BarChart from "../utils/barGraphic.jsx"
 import Doughnut from "../utils/doughnut.jsx";
 
 import TotalSales from "../components/TotalSales.jsx";
-import TotalBalance from "../components/TotalBalance.jsx";
+import StoreCard from "../components/StoreCard.jsx";
 import PendingOrders from "../components/PendingOrders.jsx";
+
+import useDataCategory from "../components/categories/hook/useDataCategory.jsx";
 
 const Dashboard = () => {
 
     const { user } = useAuth();
-    const { products, deleteProduct, updateProduct, loading, fetchData } = useUserDataProducts();
+    const dataProducts = useUserDataProducts();
+    const dataCategories = useDataCategory(); //Stores
 
     useEffect(() => {
-        fetchData();
+        dataProducts.fetchData();
     }, []);
 
     return (
@@ -65,39 +68,25 @@ const Dashboard = () => {
                             </div>
                         </div>
 
-                        <div className="d-flex justify-content-around total-balance-section">
-                            <TotalBalance
-                                image={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR73broZuXihBx0tLGP7_gE5FvPHdCoK8OMSg&s"}
-                                total={20}
-                            />
-
-                            <TotalBalance
-                                image={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQw3iIiDJQW4HYJk2osK19O3RQ4Hs4MDSF8PQ&s"}
-                                total={212}
-                            />
-                            <TotalBalance
-                                image={"https://plus.unsplash.com/premium_photo-1661876402729-09f3b7e87640?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cG5nfGVufDB8fDB8fHww"}
-                                total={510}
-                            />
-
-                            <TotalBalance
-                                image={"https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgcet2b0nXlz5OtaCJglDe2UYt0EkYazeRfslRf-qlt8b0JfQT4Uv6IzzWTpgyKj8Axmu6M2DRV20bFbSvIw-igdnQ2vLH8_ry83pVKTHEKccBj5WkGvZtGXjaW1C63FvHDi8nvCsnllW_xpaMkGd4gQAe5yg_-tw_EpPpf5mzp2XShfDudxHxwPI1-QQ/s3500-d/MamaFlor.com-Payaso-Plim-Plim-Clipart-png-transparente-18.png"}
-                                total={15}
-                            />
-                            <TotalBalance
-                                image={"https://static.vecteezy.com/system/resources/thumbnails/025/138/515/small_2x/monarch-butterfly-flying-png.png"}
-                                total={401}
-                            />
+                        <div className="d-flex justify-content-around store-card-section">
+                            {
+                                dataCategories.categories.map((cat) =>
+                                (
+                                    <StoreCard
+                                        key={cat?._id}
+                                        image={cat?.image}
+                                        name={cat?.category}
+                                        status={cat?.isActive}
+                                    />
+                                ))
+                            }
                         </div>
 
                         <div className="d-flex justify-content-between align-items-center mt-3">
                             <div className="product-list-dashboard w-100">
                                 <h4>Lista de productos</h4>
                                 <ProductsTable
-                                    products={products}
-                                    deleteProduct={deleteProduct}
-                                    updateProduct={updateProduct}
-                                    loading={loading}
+                                    {...dataProducts}
                                     isEditable={false}
                                 />
                             </div>
