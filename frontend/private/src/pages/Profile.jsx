8e857fd9b0f2bf925c84e06a66816cb645c6e103
imanollramirez <aleimanolramirez@gmail.com>
2025-07-09 20:16:59 -0600

    gif added because idk what else to add

--- frontend/private/src/pages/Profile.jsx
+++ frontend/private/src/pages/Profile.jsx
@@ -26,16 +26,30 @@ const Profile = () => {
                     <div className="col-10">
 
                         <div
-                            className="mt-5"
+                            className="mt-4"
                             style={{
                                 borderRadius: 25,
                                 padding: '20px',
-                                height: '90vh',
-                                boxShadow: '0 2px 10px rgba(0, 0, 0, 0.1)'
+                                height: '95vh',
+                                width: '90%',
+                                boxShadow: '0 1px 8px 10px rgba(0, 0, 0, 0.1)'
                             }}>
+                            <h1 className="fw-bold p-2">Perfil</h1>
                             <RegisterEmployee
-                                {...data} id={user?.id} fromProfile={true}/>
+                                {...data} id={user?.id} fromProfile={true} />
+                            <div className="footer-gif-container" style={{
+                                marginTop: '20px',
+                                textAlign: 'center',
+                                backgroundColor: '#fff'
+                            }}>
+                                <img
+                                    src="https://cdn.dribbble.com/userupload/5509318/file/original-9fcb4efd061af4c6eb3c0b056bda48d1.gif"
+                                    style={{ width: '40%', opacity: 0.9 }}
+                                />
+                            </div>
+
                         </div>
+
 
                     </div>
                 </div>
