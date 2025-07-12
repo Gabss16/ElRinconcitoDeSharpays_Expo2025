fbefd99ea4b841b55252591a2d4cb46532d7953a
imanollramirez <aleimanolramirez@gmail.com>
2025-07-11 23:51:50 -0600

    navbar responsive

--- frontend-public/src/pages/Profile.jsx
+++ frontend-public/src/pages/Profile.jsx
@@ -9,7 +9,7 @@ const Profile = () => {
 
 
   return (
-    <div className="profile-container d-flex flex-column min-vh-100">
+    <div className="profile-container">
       <main className="flex-grow-1 d-flex justify-content-start">
  
            <CardOrders/>
