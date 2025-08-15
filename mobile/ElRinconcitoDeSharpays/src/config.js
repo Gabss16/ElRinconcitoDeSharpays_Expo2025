9dc80ba07277dd2de12190666242d7d131969a83
fer <fer@example.com>
2025-08-15 08:07:34 -0600

    arreglo de api

--- mobile/ElRinconcitoDeSharpays/src/config.js
+++ mobile/ElRinconcitoDeSharpays/src/config.js
@@ -1,12 +1,2 @@
-import { Platform } from "react-native";
-
-// Puedes cambiar esta IP según el entorno
-const LOCAL_IP = "192.168.56.1";
-const ANDROID_IP = "10.10.4.195";
-
-export const API_HOST =
-  Platform.OS === "android"
-    ? `http://${ANDROID_IP}:4000`
-    : `http://${LOCAL_IP}:4000`;
-
-export const API_URL = `${API_HOST}/api`;
+// Dirección IP del servidor backend
+export const API_URL = "http://10.10.1.141:4000/api";
