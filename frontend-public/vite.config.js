dd7bd3393b4b41c0cc1e9c3f3c3e10409cac3170
Gabriela <20200543@ricaldone.edu.sv>
2025-08-18 11:45:37 -0600

    arreglo para subir a vite

--- frontend-public/vite.config.js
+++ frontend-public/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      external: [], // <- asegúrate de no excluir sweetalert2
+    },
+  },
+  optimizeDeps: {
+    include: ["sweetalert2"], // 🔥 fuerza que Vite lo incluya
+  },
+});
