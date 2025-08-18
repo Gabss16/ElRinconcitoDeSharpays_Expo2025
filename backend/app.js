0f568b253b03a0efbc735d53e3ac4551bcacb55e
Gabriela <20200543@ricaldone.edu.sv>
2025-08-18 10:36:46 -0600

    arreglo para subir a vite

--- backend/app.js
+++ backend/app.js
@@ -35,7 +35,7 @@ const app = express();
 
 app.use(
     cors({
-      origin: "https://elrinconcitodesharpays-expo2025-o2f0.onrender.com", // Dominio del cliente
+      origin: "https://el-rinconcito-de-sharpays-expo2025-five.vercel.app/", // Dominio del cliente
       credentials: true, // Permitir envío de cookies y credenciales
     })
   );
