26ae0f881a3cb9d9b64e8225bc35568fa9fa5530
Gabriela <20200543@ricaldone.edu.sv>
2025-08-18 14:30:53 -0600

    s to uppercase

--- frontend-public/src/components/ErrorAlert.jsx
+++ frontend-public/src/components/ErrorAlert.jsx
@@ -1,4 +1,4 @@
-import Swal from "sweetalert2";
+import Swal from "Sweetalert2";
 
 const ErrorAlert = (title) => {
 
--- frontend-public/src/components/QuestionAlert.jsx
+++ frontend-public/src/components/QuestionAlert.jsx
@@ -1,4 +1,4 @@
-import Swal from "sweetalert2";
+import Swal from "Sweetalert2";
 
 const QuestionAlert = (title) => {
   return Swal.fire({
