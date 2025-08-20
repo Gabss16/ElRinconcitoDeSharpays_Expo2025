cc372619847a89d7d9b26c8211317e37a2f66ce7
Gabriela <20200543@ricaldone.edu.sv>
2025-08-19 19:22:43 -0600

    cambios para permisos de cors

--- backend/app.js
+++ backend/app.js
@@ -37,7 +37,7 @@ const app = express();
 
 app.use(
     cors({
-      origin: ["https://el-rinconcito-de-sharpays-expo2025.vercel.app/", "https://el-rinconcito-de-sharpays-expo2025-five.vercel.app/"], // Dominio del cliente
+      origin: ["https://el-rinconcito-de-sharpays-expo2025.vercel.app", "https://el-rinconcito-de-sharpays-expo2025-five.vercel.app"], // Dominio del cliente
       credentials: true, // Permitir envío de cookies y credenciales
     })
   );
